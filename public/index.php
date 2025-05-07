73769ed9320a322a69bc9b5b69f18b423ea5a6c0
Gleb Kaluzki <kaluzkigleb@gmail.com>
2025-05-07 16:33:08 +0200

    #1 Add a button

--- public/index.php
+++ public/index.php
@@ -8,8 +8,15 @@ $textArea = <<<HTML
     <title>Time-Management</title>
 </head>
 <body style= "display: grid; place-items: center;" marginheight = "100vp">
-    <label for = "Time imput">Time imput</label>
-    <textarea name = "Time imput" id = "Time imput" cols = "130" rows = "40" placeholder = "Paste your time table here..."></textarea>
+    <section class="wrapper-main">
+        <form action = "index.php" method = "post">
+            <label for = "Time imput">Time imput</label>
+            <br>
+            <textarea name = "Time imput" id = "Time imput" cols = "130" rows = "40" placeholder = "Paste your time table here..."></textarea>
+    
+            <button type="submit" value="submit">Submit text</button>
+        </form>
+    </section>
 </body>
 HTML;
 echo $textArea;
